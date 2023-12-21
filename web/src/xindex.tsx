082a94061c092367a045920dd15c5b16f1195fa5
xuedan <32359500@qq.com>
2023-12-21 14:43:26 +0800

    X

---
+++ web/src/xindex.tsx
@@ -0,0 +1,6 @@
+
+export {default as XLogin} from "./pages/主框架/登录"
+export {default as XMain} from "./pages/主框架/主界面"
+export {default as XUser} from "./pages/主框架/用户详情"
+export {default as AdminMenu} from "./pages/WebMenu"
+
