d5d6c06182dddaf6898bbc5a7f63c0bd70f796ba
tangbin <yvhk2900@gmail.com>
2023-12-25 15:48:12 +0800

    update

--- web/src/pages/系统管理/系统配置.js
+++ web/src/pages/系统管理/系统配置.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types'
-import {XUpload, XBasePage, XButton, XCard, XFlex, XDivider, XForm, XGrid, XInput, XModal, XPopConfirm, XRadioGroup, XTableGrid, XMessage} from "xdcoreweb";
+import {XUpload, XBasePage, XButton, XCard, XFlex, XDivider, XForm, XGrid, XInput, XModal, XPopConfirm, XRadioGroup, XTableGrid, XBaseApp} from "xdcoreweb";
 
 //@menu 系统配置
 export default class 系统配置 extends XBasePage {
@@ -16,25 +16,16 @@ export default class 系统配置 extends XBasePage {
   constructor(props) {
     super(props);
     this.state.visibleColumns = [
-      {field: "配置类型", keyword: true,},
       {field: "配置项", keyword: true,},
-      {
-        field: "配置值", render: (text, record) => {
-          if (record.配置类型 === "模板配置") {
-            return <XUpload readOnly={true} value={text} downloadUrl={"download_file"}/>
-          }
-          return <div>{text}</div>
-        }
-      },
+      {field: "配置值"},
       {
         title: '操作', field: '操作', width: 200, visible: this.CheckOperation("编辑"),
         render: (text, record) => {
           return <XFlex>
             <XButton isA={true} onClick={() => this.showSaveModal(record)} text={"修改"}/>
             <XDivider/>
-            <XPopConfirm width={"auto"} title="是否删除以下所有记录?" onOK={() => {
-              this.DeleteTableData('xtpz/delete', record, this.table);
-            }}>
+            <XPopConfirm width={"auto"} title="是否删除以下所有记录?"
+                         onOK={() => this.DeleteTableData('xtpz/delete', record, this.table)}>
               <XButton isA={true} text={"删除"}/>
             </XPopConfirm>
           </XFlex>
@@ -51,36 +42,17 @@ export default class 系统配置 extends XBasePage {
     const Ele = <XForm infoUrl={"xtpz/queryinfo"} useServerInfo={false} infoData={data} inited={(e) => this.form = e}>
       <XGrid columnGap={"10px"} rowGap={"10px"} rowsTemplate={["auto"]}>
         <XInput field={"id"} visible={false} parent={() => this.form}/>
-        <XRadioGroup labelWidth={labelWidth} value={"系统配置"}
-                     isRequired={true} label={"配置类型"} field={"配置类型"}
-                     items={["系统配置", "数据字典"]}
-                     parent={() => this.form}/>
         <XInput labelWidth={labelWidth} field={"配置项"} isRequired={true} parent={() => this.form}/>
         <XInput labelWidth={labelWidth} field={"配置值"} parent={() => this.form}/>
-        {/*<XUpload labelWidth={lableWidth} visible={false} field={"配置值"}*/}
-        {/*         maxFileNum={1} maxFileSize={20} uploadUrl={"xtpz/upload"} downloadUrl={"download_file"}*/}
-        {/*         fileType={XUpload.FileType.excel}*/}
-        {/*         parent={() => this.form}/>*/}
       </XGrid>
     </XForm>;
     XModal.ModalShow("配置信息", async () => {
-      let p = this.form.GetEditorValues()
-      if (p && !p.id && p.配置类型 && p.配置项) {
-        p.id = p.配置类型 + p.配置项;
-        let ret = await this.RequestServer("xtpz/save", {...p});
-        if (ret.Success) {
-          this.table.Refresh();
-          return true;
-        } else {
-          return false;
-        }
-      }
       return this.SaveFormData(this.form, "xtpz/save", this.table, {});
     }, Ele, '950px',);
   }
 
-  download(){
-
+  download() {
+    XBaseApp.DownloadDomainModel();
   }
 
 
