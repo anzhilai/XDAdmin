8df243f184dc3a867a4946e494e1edcf2cc6fde7
tangbin <yvhk2900@gmail.com>
2024-01-03 15:52:02 +0800

    update

--- src/main/java/com/anzhilai/admin/XDAdminApplication.java
+++ src/main/java/com/anzhilai/admin/XDAdminApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.servlet.ServletContext;
+
 @SpringBootApplication
 @EnableScheduling//开启定时任务功能
 @EnableTransactionManagement
@@ -18,6 +20,10 @@ public class XDAdminApplication extends BaseApplication {
         return new String[]{"com.anzhilai"};
     }
 
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        System.out.println("作为类库时不启动");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(XDAdminApplication.class, args);
